50d89ed044cdeb89cffb57469af074ca05ba6420
jbz9 <3390658300@qq.com>
2022-05-30 20:48:26 +0800

    init

---
+++ src/test/java/com/demo/springcloudalibabademo/SpringCloudAlibabaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springcloudalibabademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
